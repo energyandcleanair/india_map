8eced9a0b1c50c22095b35a56c4032b1cd94db3f
rosa <rosa@energyandcleanair.org>
2025-03-25 16:07:38 +0200

    Fix imports

--- 02_first_stage_ML/CO_impute.py
+++ 02_first_stage_ML/CO_impute.py
@@ -17,7 +17,8 @@ import math
 import random
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import GroupKFold, GridSearchCV, shuffle
+from sklearn.model_selection import GroupKFold, GridSearchCV
+from sklearn.utils import shuffle
 from sklearn.metrics import r2_score, mean_squared_error
 from xgboost import XGBRegressor
 
