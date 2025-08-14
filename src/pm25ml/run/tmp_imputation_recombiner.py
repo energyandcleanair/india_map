9deb63071741e390758be8fac10369f7e8e06d00
Panda Rushwood <panda@energyandcleanair.org>
2025-08-14 14:27:51 +0100

    Fix generated from temp imputation recombiner

--- src/pm25ml/run/tmp_imputation_recombiner.py
+++ src/pm25ml/run/tmp_imputation_recombiner.py
@@ -10,12 +10,16 @@ from pm25ml.setup.dependency_injection import Pm25mlContainer, init_dependencies
 @inject
 def _main(
     imputation_recombiner: Recombiner = Provide[Pm25mlContainer.imputer_recombiner],
+    generated_features_artifact: DataArtifactRef = Provide[
+        Pm25mlContainer.data_artifacts_container.generated_features_stage
+    ],
     imputed_data_artifact: DataArtifactRef = Provide[
         Pm25mlContainer.data_artifacts_container.ml_imputed_super_stage
     ],
 ) -> None:
     imputation_recombiner.recombine(
         stages=[
+            generated_features_artifact,
             imputed_data_artifact.for_sub_artifact("aod"),
             imputed_data_artifact.for_sub_artifact("no2"),
             imputed_data_artifact.for_sub_artifact("co"),
