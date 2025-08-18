8cd14d8a79378f92f5bf12a4fd1ece42846badcc
Panda Rushwood <panda@energyandcleanair.org>
2025-08-18 14:34:26 +0100

    Improve naming for steps

--- src/pm25ml/run/_run_local.py
+++ src/pm25ml/run/_run_local.py
@@ -4,15 +4,15 @@ from runpy import run_module
 
 
 def _main() -> None:
-    run_module("pm25ml.run.fetch_and_combine", run_name="__main__")
-    run_module("pm25ml.run.generate_features", run_name="__main__")
-    run_module("pm25ml.run.sample_for_imputation", run_name="__main__")
-    run_module("pm25ml.run.train_aod_imputer", run_name="__main__")
-    run_module("pm25ml.run.train_co_imputer", run_name="__main__")
-    run_module("pm25ml.run.train_no2_imputer", run_name="__main__")
-    run_module("pm25ml.run.impute", run_name="__main__")
-    run_module("pm25ml.run.prep_for_full_model", run_name="__main__")
-    run_module("pm25ml.run.train_full_model", run_name="__main__")
+    run_module("pm25ml.run.s01_fetch_and_combine", run_name="__main__")
+    run_module("pm25ml.run.s02_generate_features", run_name="__main__")
+    run_module("pm25ml.run.s03_sample_for_imputation", run_name="__main__")
+    run_module("pm25ml.run.s04_train_aod_imputer", run_name="__main__")
+    run_module("pm25ml.run.s04_train_co_imputer", run_name="__main__")
+    run_module("pm25ml.run.s04_train_no2_imputer", run_name="__main__")
+    run_module("pm25ml.run.s05_impute", run_name="__main__")
+    run_module("pm25ml.run.s06_prep_for_full_model", run_name="__main__")
+    run_module("pm25ml.run.s07_train_full_model", run_name="__main__")
 
 
 if __name__ == "__main__":
